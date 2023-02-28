6f5d94a21dab397f91ff472a13a888418de8a733
kennywindis <kennyphilip100@gmail.com>
2023-02-28 05:41:50 -0800

    api2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Module with top_ten function"""
-import requests
-import sys
+
+from json import loads
+from requests import get
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit."""
+    """queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'limit': 10}
-    json_obj = requests.get(
-        url, headers={'User-Agent': 'My User Agent 1.0'}, params=parameters)
-    if json_obj.status_code != 404:
-        dict_obj = json_obj.json().get('data').get('children')
-        for each in dict_obj:
-            print(each.get('data').get('title'))
-    else:
-        print(None)
-
+    try:
+        children = reddits.get('data').get('children')
+        for i in range(10):
+            print(children[i].get('data').get('title'))
+    except:
+        print('None')
